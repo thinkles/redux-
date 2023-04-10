a7f478294c7c862032754458e18c6497d57ebe73
杨祥骐 <yang.xq@luban.com>
2023-04-10 15:45:58 +0800

    feat:"first"

---
+++ src/types/type.d.ts
@@ -0,0 +1,16 @@
+import {AxiosRequestConfig, AxiosRequestHeaders} from "axios";
+
+
+declare global {
+    interface CustomAxiosRequestConfig<D = any> extends AxiosRequestConfig<D> {
+     showLoading:boolean
+    }
+
+  }
+
+  
+  declare module 'axios' {
+    export interface InternalAxiosRequestConfig<D = any> extends CustomAxiosRequestConfig<D> {
+      headers: AxiosRequestHeaders;
+    }
+  }
---
+++ src/utils/loading.tsx
@@ -0,0 +1,24 @@
+import {Spin} from "antd"
+import ReactDOM from "react-dom"
+
+// 当前正在请求的数量
+let requestCount = 0
+
+// 显示loading
+export function showLoading() {
+    if (requestCount === 0) {
+        var dom = document.createElement('div')
+        dom.setAttribute('id', 'loading')
+        document.body.appendChild(dom)
+        ReactDOM.render(<Spin tip="加载中..." size = "large" />, dom)
+    }
+    requestCount++
+}
+
+// 隐藏loading
+export function hideLoading() {
+    requestCount--
+    if (requestCount === 0) {
+        document.body.removeChild(document.getElementById('loading') as HTMLElement )
+    }
+}
